7faca9743d8a71c77ea3f6c830eda0046a1da623
Alex1iv <ivanov.politehnik@gmail.com>
2023-06-11 17:04:24 +0600

    initial commit

---
+++ models/models.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import Model
+
+# Импорт параметров
+# from utils.functions import config_reader
+
+# config = config_reader()
+
---
+++ utils/models_utils.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import random
+import tensorflow as tf
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+
