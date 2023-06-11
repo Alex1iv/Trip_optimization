4c1a62197ca316218ab2e4eb449a23abcafbb947
Alex1iv <ivanov.politehnik@gmail.com>
2023-06-11 17:03:02 +0600

    deleted

--- models/models.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from keras.models import Model
-
-# Импорт параметров
-# from utils.functions import config_reader
-
-# config = config_reader()
-
--- utils/models_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import random
-import tensorflow as tf
-from keras import backend as K
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-
-
-
